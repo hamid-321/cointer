7e1e7fb513eb6da65153b6ee3cbd198bc4291816
hamid-321 <hamidc516@gmail.com>
2026-02-02 21:03:29 +0000

    feat: added user auth and login, dynamic nav bar too. Also added another fix for db connection dying

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'attr' => ['autocomplete' => 'email'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter an email address',
+                    ]),
+                    new Email([
+                        'message' => 'Please enter a valid email address',
+                    ]),
+                    new Length([
+                        'max' => 180,
+                        'maxMessage' => 'Email cannot be longer than {{ limit }} characters',
+                    ]),
+                ],
+            ])
+            ->add('displayName', TextType::class, [
+                'label' => 'Display Name',
+                'attr' => ['autocomplete' => 'name'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a display name',
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'minMessage' => 'Display name must be at least {{ limit }} characters',
+                        'max' => 30,
+                        'maxMessage' => 'Display name cannot be longer than {{ limit }} characters',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'mapped' => false,
+                'invalid_message' => 'The password fields must match.',
+                'first_options'  => [
+                    'label' => 'Password',
+                    'attr' => ['autocomplete' => 'new-password'],
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Please enter a password',
+                        ]),
+                        new Length([
+                            'min' => 6,
+                            'minMessage' => 'Your password should be at least {{ limit }} characters',
+                            'max' => 4096,
+                        ]),
+                    ],
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                    'attr' => ['autocomplete' => 'new-password'],
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
