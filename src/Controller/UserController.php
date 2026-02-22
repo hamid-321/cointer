6d507cbc3896261fdaa642f0423d2da0b2e93c6f
hamid-321 <hamidc516@gmail.com>
2026-02-22 00:58:44 +0000

    Added change password functionality, updated password requirements to be more strict and introduced rate limiting to login attempts

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -12,6 +12,9 @@ use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Form\FormError;
+use App\Form\ChangePasswordType;
 
 #[IsGranted('ROLE_USER')]
 final class UserController extends AbstractController
@@ -73,5 +76,55 @@ final class UserController extends AbstractController
         }
         
         return $this->redirectToRoute('app_coin_index');
+    }
+
+    #[Route('/profile/change-password', name: 'app_user_change_password', methods: ['GET', 'POST'])]
+    public function changePassword(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, TokenStorageInterface $tokenStorage): Response
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(ChangePasswordType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) 
+        {
+            $currentPassword = $form->get('current_password')->getData();
+            $newPassword = $form->get('new_password')->getData();
+            
+            if (!$userPasswordHasher->isPasswordValid($user, $currentPassword)) 
+            {
+                $form->get('current_password')->addError(new FormError('The current password is incorrect.'));
+                return $this->render('user/change_password.html.twig', [
+                    'form' => $form,
+                    'user' => $user,
+                ]);
+            }
+
+            if ($newPassword === $currentPassword) 
+            {
+                $form->get('current_password')->addError(new FormError('The new password cannot be the same as the current password.'));
+                return $this->render('user/change_password.html.twig', [
+                    'form' => $form,
+                    'user' => $user,
+                ]);
+            }
+            
+            $user->setPassword($userPasswordHasher->hashPassword($user, $newPassword));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $token = new UsernamePasswordToken($user, 'main', $user->getRoles());
+            $tokenStorage->setToken($token);
+
+            $this->addFlash('success', 'Your password has been changed.');
+
+            return $this->redirectToRoute('app_user_profile');
+        }
+
+        return $this->render('user/change_password.html.twig', [
+            'form' => $form,
+            'user' => $user,
+        ]);
     }
 }
--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -4,71 +4,46 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
 
-class RegistrationFormType extends AbstractType
+class ChangePasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email', EmailType::class, [
-                'attr' => ['autocomplete' => 'email'],
+            ->add('current_password', PasswordType::class, [
+                'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter an email address',
-                    ]),
-                    new Email([
-                        'message' => 'Please enter a valid email address',
-                    ]),
-                    new Length([
-                        'max' => 180,
-                        'maxMessage' => 'Email cannot be longer than {{ limit }} characters',
+                        'message' => 'Please enter your current password',
                     ]),
                 ],
             ])
-            ->add('displayName', TextType::class, [
-                'label' => 'Display Name',
-                'attr' => ['autocomplete' => 'name'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a display name',
-                    ]),
-                    new Length([
-                        'min' => 2,
-                        'minMessage' => 'Display name must be at least {{ limit }} characters',
-                        'max' => 30,
-                        'maxMessage' => 'Display name cannot be longer than {{ limit }} characters',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', RepeatedType::class, [
+            ->add('new_password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'mapped' => false,
                 'invalid_message' => 'The password fields must match.',
                 'first_options'  => [
-                    'label' => 'Password',
+                    'label' => 'New password',
                     'attr' => ['autocomplete' => 'new-password'],
                     'constraints' => [
                         new NotBlank([
-                            'message' => 'Please enter a password',
+                            'message' => 'Please enter a new password',
                         ]),
                     ],
                 ],
                 'second_options' => [
-                    'label' => 'Repeat Password',
+                    'label' => 'Confirm new password',
                     'attr' => ['autocomplete' => 'new-password'],
                     'constraints' => [
                         new NotBlank([
-                            'message' => 'Please confirm your password',
+                            'message' => 'Please confirm your new password',
                         ]),
                         new Length([
                             'min' => 8,
@@ -91,6 +66,7 @@ class RegistrationFormType extends AbstractType
                     ],
                 ],
             ])
+
         ;
     }
 
